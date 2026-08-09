8475cef4eae437c4cf919049ee29a88ba169d77c
newkrishna983-lang <newkrishna983@gmail.com>
2026-08-09 23:04:03 +0530

    Refactor API configuration and message formats

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "34422904"))
 API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8915320693:AAF6a35ytSzCy99ZcLk0tLkdTOFWQXQzhVM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8915320693:AAHfryZYdCxVpmFTG0DbDTkDxzHt9XN2wY8")
 
 CREDIT = os.environ.get("CREDIT", "•Ɱའ★Krΐຮhήส•❤️‍🔥")
 # MongoDB Configuration
