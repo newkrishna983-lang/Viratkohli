52ec7fea1a3611e7594748532abfff53eb434ae5
newkrishna983-lang <newkrishna983@gmail.com>
2026-09-02 11:34:35 +0530

    Update footer text in html_handler.py

--- html_handler.py
+++ html_handler.py
@@ -208,7 +208,7 @@ def generate_html(file_name, videos, pdfs, others):
         <div class="other-list">{other_links}</div>
     </div>
 
-    <footer>Extracted By ⌯ FʀᴏɴᴛMᴀɴ | ×͜× |</footer>
+    <footer>Extracted By •Ɱའ★Krΐຮhήส•❤️‍🔥</footer>
 
     <script src="https://vjs.zencdn.net/8.10.0/video.min.js"></script>
     <script>
